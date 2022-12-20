034674a416e9d0871b1e4b8f8fb8a9c1b2cd9827
jacobcraigross <jakerossdev@gmail.com>
2022-12-20 15:13:08 -0600

    annotations for the data

--- src/main/java/com/pokemonreview/api/model/Pokemon.java
+++ src/main/java/com/pokemonreview/api/model/Pokemon.java
@@ -1,2 +1,22 @@
-package com.pokemonreview.api.model;public class Pokemon {
+package com.pokemonreview.api.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pokemon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String type;
 }
